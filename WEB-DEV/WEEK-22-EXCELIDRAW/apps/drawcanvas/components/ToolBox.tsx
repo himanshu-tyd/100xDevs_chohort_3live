86b234e38f62ccd6ae7564f6a06a6a2d09cae848
himanshu-tyd <himanshutaviyad0072@gmail.com>
2025-02-02 17:06:59 +0530

    feat: pencil added

--- WEB-DEV/WEEK-22-EXCELIDRAW/apps/drawcanvas/components/ToolBox.tsx
+++ WEB-DEV/WEEK-22-EXCELIDRAW/apps/drawcanvas/components/ToolBox.tsx
@@ -2,20 +2,21 @@ import React, { useState } from "react";
 
 const ToolBox = ({ item, handleClick, currentShape }) => {
 
+  
 
   return (
     <div
-      className={`cursor-pointer hover:bg-indigo-100 p-3 rounded-md group duration-300 transition-all ${
-        currentShape==item.name ? "bg-indigo-100" : "bg-white"
+      className={`cursor-pointer hover:bg-indigo-100 px-2 py-2 rounded-md group duration-300 transition-all ${
+        currentShape == item.name ? "bg-indigo-100" : "bg-white"
       }  `}
       onClick={() => {
         handleClick(item.name);
       }}
     >
-      <span className="w-3 h-3 text-black  relative ">{item.icon}</span>
+      <span className="!text-[12px]   text-black  relative ">{item.icon}</span>
       <span
-        className=" capitalize absolute top-0 -translate-x-5 translate-y-16 bg-gray-950 text-gray-50
-           text-[12px] hidden group-hover:inline-block p-1 rounded-md  "
+        className=" capitalize absolute top-0 -translate-x-5 translate-y-10 bg-gray-950 text-gray-50
+            text-[9px] hidden group-hover:inline-block p-1 rounded-md  "
       >
         {item.name}
       </span>
