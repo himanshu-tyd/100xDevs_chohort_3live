72671f3b4c7b0af30a38d4e0772da61d98492d8b
himanshu-tyd <himanshutaviyad0072@gmail.com>
2025-03-22 23:01:44 +0530

    learn: promoutheus

--- DEV0PS/WEEK-28-SCALING/index.js
+++ DEV0PS/WEEK-28-SCALING/index.js
@@ -7,8 +7,8 @@ const totalCPUs = os.cpus().length;
 const port = 3000;
 
 if (cluster.isPrimary) {
-  console.log(`Number of CPUs is ${totalCPUs}`);
-  console.log(`Primary ${process.pid} is running`);
+    console.log(`Number of CPUs is ${totalCPUs}`);
+    console.log(`Primary ${process.pid} is running`);
 
   // Fork workers.
   for (let i = 0; i < totalCPUs; i++) {
